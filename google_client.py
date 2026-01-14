bc9b88a70b0ec69b492ff01d4e52a49080e51d80
almost-bully <almost.bully13@gmail.com>
2026-01-14 13:15:37 +0300

    use service account from env

--- google_client.py
+++ google_client.py
@@ -1,15 +1,18 @@
-# google_client.py
-
+import json
+import os
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
-from config import SERVICE_ACCOUNT_FILE
 
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
 def get_sheets_service():
-    credentials = Credentials.from_service_account_file(
-        SERVICE_ACCOUNT_FILE,
+    service_account_info = json.loads(
+        os.environ["GOOGLE_SERVICE_ACCOUNT_JSON"]
+    )
+
+    credentials = Credentials.from_service_account_info(
+        service_account_info,
         scopes=SCOPES
     )
-    service = build("sheets", "v4", credentials=credentials)
-    return service
+
+    return build("sheets", "v4", credentials=credentials)
